c681eff17eed362f155c19b6e255a84e9de3f150
kayesIbnaQayum <kayes.qayum@gmail.com>
2019-05-19 17:50:12 +0600

    added more features in top Menu

--- resources/views/menu/topMenu.blade.php
+++ resources/views/menu/topMenu.blade.php
@@ -1,10 +1,19 @@
                     <div class="row">
                       
                             <a href="{{route('patientSession')}}" class="btn col-sm-2" style="padding:20px; margin: 10px; border-color: blue">Add Session</a>
-                    <a  class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color: blue">Paymenmts</a>       
+                    <a  class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color: blue">Add Paymenmts</a>       
                     <a class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color: blue">Add Rate</a>
                     <a  class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color: blue">Add Patient</a>
                     <a  class="btn col-sm-2" style="padding:20px; margin: 10px; border-color: blue">Add Doctor</a>
 
+
+                    </div>
+                    <div class="row">
+                      
+                            <a href="{{route('patientSession')}}" class="btn col-sm-2" style="padding:20px; margin: 10px; border-color: blue">Session List</a>
+                    <a  class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color: blue">Payment list</a>       
+                    <a  class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color: blue">Patient List</a>
+                    <a  class="btn col-sm-2" style="padding:20px; margin: 10px; border-color: blue">Doctor List</a>
+                    <a class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color:red">Monthly <br>Calculation</a>
 
                     </div>
