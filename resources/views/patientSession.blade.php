1b6fb6421ffb7b94607ab6d3fd279a8799f4d414
kayesIbnaQayum <kayes.qayum@gmail.com>
2019-05-19 17:50:37 +0600

    Patient Session page done

--- resources/views/patientSession.blade.php
+++ resources/views/patientSession.blade.php
@@ -17,6 +17,47 @@
                     <!--Component-->
                     @include('menu/topMenu')
 
+                    <div class="row">
+                        <form action="/action_page.php" class="col-sm-12">
+                              <label for="sel1"><span style="color:red">Patient</span> NAME and ID</label>
+                                  <select class="form-control" name="patient_id">
+                                    <option value="20">1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                  </select><br>
+
+                              <label for="sel1">Therapist NAME and ID</label>
+                                  <select class="form-control" id="sel1">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                  </select><br>
+
+
+                              <label for="sel1">Session Time</label>
+                                  <select class="form-control" id="sel1">
+                                    <option>Morning</option>
+                                    <option>Evening</option>
+                                    <option>Night</option>
+                                  </select><br>
+                    
+                     <label >Session Date</label>
+                     <input type="date" name="bday" max="3000-12-31" 
+                            min="1000-01-01" class="form-control"><br>
+
+                    <label >Rate</label>    
+                     <input type="text" class="form-control" name="rate"><br>
+
+                    <label >Paid</label>    
+                     <input type="text" class="form-control" name="paid"><br>            
+
+                     <input type="submit">
+                 </form>
+
+                    </div>
+
                 </div>
             </div>
         </div>
