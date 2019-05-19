44caed7edc85172d5dc0609f9065859cf0fe564e
kayesIbnaQayum <kayes.qayum@gmail.com>
2019-05-19 17:27:45 +0600

    patientSession Page created and top menu added to patientsession page, home page

---
+++ resources/views/menu/topMenu.blade.php
@@ -0,0 +1,10 @@
+                    <div class="row">
+                      
+                            <a href="{{route('patientSession')}}" class="btn col-sm-2" style="padding:20px; margin: 10px; border-color: blue">Add Session</a>
+                    <a  class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color: blue">Paymenmts</a>       
+                    <a class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color: blue">Add Rate</a>
+                    <a  class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color: blue">Add Patient</a>
+                    <a  class="btn col-sm-2" style="padding:20px; margin: 10px; border-color: blue">Add Doctor</a>
+
+
+                    </div>
