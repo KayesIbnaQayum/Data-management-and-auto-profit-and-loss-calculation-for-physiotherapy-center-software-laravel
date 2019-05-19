5f0675ded7771cb279ce23f133d6c48c5ed997f1
kayesIbnaQayum <kayes.qayum@gmail.com>
2019-05-19 20:40:19 +0600

    sessionView and PatientInfoListView Controller function added

---
+++ app/Http/Controllers/PostViewController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\patient_session as Psession;
+use App\patientListView as patientListView;
+
+class PostViewController extends Controller
+{
+    //
+
+    public function view_session(){
+    	$data = Psession::paginate(20);
+    	
+    	return view('sessionView')->with('data', $data);
+    }
+
+
+    public function view_patientList(){
+    	$data = patientListView::paginate(20);
+    	
+    	return view('patientListView')->with('data', $data);
+    }
+
+    
+
+}
