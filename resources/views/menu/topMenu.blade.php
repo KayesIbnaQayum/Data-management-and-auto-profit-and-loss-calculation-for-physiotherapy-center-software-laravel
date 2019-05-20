1e5f226f8e6c425797ad0c69c5a26b9173b1924c
kayesIbnaQayum <kayes.qayum@gmail.com>
2019-05-20 10:30:36 +0600

    added payment pages link in top menu

--- resources/views/menu/topMenu.blade.php
+++ resources/views/menu/topMenu.blade.php
@@ -1,7 +1,7 @@
                     <div class="row">
                       
                             <a href="{{route('patientSession.create')}}" class="btn col-sm-2" style="padding:20px; margin: 10px; border-color: blue">Add Session</a>
-                    <a href="" class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color: blue">Add Paymenmts</a>       
+                    <a href="{{route('payment.create')}}" class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color: blue">Add Paymenmts</a>       
                     <a href="{{route('patient.create')}}" class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color: blue">Add Patient</a>
                     <a href="{{route('doctor.create')}}" class="btn col-sm-2" style="padding:20px; margin: 10px; border-color: blue">Add Doctor</a>
                     <a class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color:blue; border-color:red">Details</a>
@@ -10,7 +10,7 @@
                     <div class="row">
                       
                             <a href="{{route('patientSession.index')}}" class="btn col-sm-2" style="padding:20px; margin: 10px; border-color: blue">Session List</a>
-                    <a href="" class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color: blue">Payment list</a>       
+                    <a href="{{route('patientSession.index')}}" class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color: blue">Payment list</a>       
                     <a href="{{route('patient.index')}}" class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color: blue">Patient List</a>
                     <a href="{{route('doctor.index')}}" class="btn col-sm-2" style="padding:20px; margin: 10px; border-color: blue">Doctor List</a>
                     <a class="btn col-sm-2" style=" padding:20px; margin: 10px; border-color:red">Monthly <br>Calculation</a>
